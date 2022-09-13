f53a8b3e1ab06d92f9ccd0b39702afe31c610e12
Daniil Bodryagin <daniilMAOmail@gmail.com>
2022-09-13 10:24:44 +0300

    Tiny JS-world first commit

--- index.js
+++ index.js
@@ -1,30 +1,63 @@
 import { print } from './js/lib.js';
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
+const dog = {
+   hands: 0,
+   legs: 4,
+   name: 'Barbos',
+   gender: 'male',
+   phrase: 'Woof-woof!',
+   species: 'dog',
+}
 
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+const cat = {
+   hands: 0,
+   legs: 4,
+   name: 'Sonya',
+   gender: 'female',
+   phrase: 'Meow!',
+   species: 'cat'
+}
 
+const woman = {
+   hands: 2,
+   legs: 2,
+   name: 'Eve',
+   gender: 'female',
+   phrase: 'Hello! Glad to see you!',
+   species: 'human'
+}
 
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
+const man = {
+   hands: 2,
+   legs: 2,
+   name: 'Adam',
+   gender: 'male',
+   phrase: 'Hi! Today is cool!',
+   species: 'human'
+}
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+const catwoman = {
+   hands: 2,
+   legs: 2,
+   name: 'Selina',
+   gender: 'female',
+   species: 'human'
+}
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
+catwoman.phrase = cat.phrase;
 
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+const friendsList = [
+   {inhabitant: dog, friends: [woman, man]},
+   {inhabitant: cat, friends: [woman]},
+   {inhabitant: woman, friends: [dog, cat, man]},
+   {inhabitant: man, friends: [dog, woman]},
+   {inhabitant: catwoman, friends: [cat, man]}
+]
+
+friendsList.forEach(({inhabitant, friends}) => 
+                     inhabitant.friends = friends.map(({name}) => name).join(', '))
+
+const inhabitants = [dog, cat, woman, man, catwoman];
+inhabitants.map(({species, name, gender, hands, legs, phrase, friends}) => 
+               [species, name, gender, hands, legs, phrase, friends].join('; '))
+           .forEach(message => print(message));
